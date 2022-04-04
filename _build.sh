80c7ddf6624b80d07d77101fbf85a3000b153858
Wouter Van Rossem <wouter@woutervanrossem.eu>
2022-04-04 16:40:38 +0200

    Add pdf output template based on Amsterdown
    https://lcreteig.github.io/amsterdown/index.html

--- _build.sh
+++ _build.sh
@@ -2,6 +2,6 @@
 
 set -ev
 
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::markdown_document2')"
